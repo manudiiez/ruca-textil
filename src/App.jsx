586da63fc4064d39ffdd88f7968abe2ba05a1c4e
agupeiretti <agupeiretti@gmail.com>
2022-12-13 11:44:40 -0300

    13/12 agus

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Menu from './components/Menu/Menu';
 import Home from './pages/home/Home';
 
@@ -8,6 +9,7 @@ function App() {
   return (
    <>
     <Menu/>
+    <Footer/>
     <Routes>
       <Route path='/' element={<Home/>} />
     </Routes>
