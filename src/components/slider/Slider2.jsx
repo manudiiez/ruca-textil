d82c82cb3bb8abaec015ba6d766acaf1ef08dfa1
manudiiez <manudiiez123@gmail.com>
2022-12-14 00:35:13 -0300

    slider ready

--- src/components/slider/Slider2.jsx
+++ src/components/slider/Slider2.jsx
@@ -1,47 +1,24 @@
 import React from 'react'
 import "./slider2.css"
-import estampa1 from "../../assets/ESTAMPA1.png"
-import estampa2 from "../../assets/ESTAMPA2.png"
-import estampa3 from "../../assets/ESTAMPA3.png"
-import estampa4 from "../../assets/ESTAMPA4.png"
-import estampa5 from "../../assets/ESTAMPA5.png"
-import estampa6 from "../../assets/ESTAMPA6.png"
-import estampa7 from "../../assets/ESTAMPA7.png"
-import mokshort from "../../assets/mokshort.png"
-import { motion } from 'framer-motion'
+ import { motion } from 'framer-motion'
 
-const Slider2 = () => {
+const Slider2 = ({ base, prints }) => {
   return (
     <div id='sliderStamp'>
-        <div className='oval'></div>
-        <motion.div className='sliderContainer'>
-          <img className='a1' src={mokshort} alt="" />
-          <motion.div className='slider' drag="x" dragConstraints={{ right: 820, left: -820 }}>
-
-            <motion.div className='item'>
-              <img src={estampa1} alt="" />
-            </motion.div>
-            <motion.div className='item'>
-              <img src={estampa2} alt="" />
-            </motion.div>
-            <motion.div className='item'>
-              <img src={estampa3} alt="" />
-            </motion.div>
-            <motion.div className='item'>
-              <img src={estampa4} alt="" />
-            </motion.div>
-            <motion.div className='item'>
-              <img src={estampa5} alt="" />
-            </motion.div>
-            <motion.div className='item'>
-              <img src={estampa6} alt="" />
-            </motion.div>
-            <motion.div className='item'>
-              <img src={estampa7} alt="" />
-            </motion.div>
-          </motion.div>
+      <div className='oval'></div>
+      <motion.div className='sliderContainer'>
+        <img className='a1' src={base} alt="" />
+        <motion.div className='slider' drag="x" dragConstraints={{ right: 820, left: -820 }}>
+          {
+            prints.map(img => (
+              <motion.div className='item'>
+                <img src={img} alt="" />
+              </motion.div>
+            ))
+          }
         </motion.div>
-        <div className='oval2'></div>
+      </motion.div>
+      <div className='oval2'></div>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,6 +8,26 @@ import feature2 from '../../assets/feature2.svg'
 import feature3 from '../../assets/feature3.svg'
 import wsp from '../../assets/ri_whatsapp-fill.svg'
 
+// SLIDER
+import estampa1 from "../../assets/ESTAMPA1.png"
+import estampa2 from "../../assets/ESTAMPA2.png"
+import estampa3 from "../../assets/ESTAMPA3.png"
+import estampa4 from "../../assets/ESTAMPA4.png"
+import estampa5 from "../../assets/ESTAMPA5.png"
+import estampa6 from "../../assets/ESTAMPA6.png"
+import estampa7 from "../../assets/ESTAMPA7.png"
+import mokshort from "../../assets/mokshort.png"
+
+const imgs = [
+  estampa1,
+  estampa2,
+  estampa3,
+  estampa4,
+  estampa5,
+  estampa6,
+  estampa7,
+]
+
 const Home = () => {
   return (
     <div id='home'>
@@ -17,7 +37,7 @@ const Home = () => {
           <img src={banner} alt="" />
           <p>MARCÁ LA DIFERENCIA</p>
         </div>
-        <Slider2 />
+        <Slider2 base={mokshort} prints={imgs} />
         <div className='homeDesc'>
           <p className='homeDescTitle'>FABRICAMOS INDUMENTARIA A TU MEDIDA</p>
           <p className='homeDescSubtitle'>Emprendé con un mínimo de 10 prendas, sin mínimo de talles y sin mínimo de color </p>
