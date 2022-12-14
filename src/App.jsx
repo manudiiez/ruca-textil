e9e5768491e6901a7039593586d3cf7d9bf49f15
agupeiretti <agupeiretti@gmail.com>
2022-12-14 11:02:43 -0300

    14/12 2 agus

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import Menu from './components/Menu/Menu';
 import Home from './pages/home/Home';
+import Nosotros from './pages/nosotros/Nosotros';
 import Productos from './pages/productos/Productos';
 
 
@@ -15,6 +16,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/productos' element={<Productos/>} /> 
+      <Route path='/nosotros' element={<Nosotros/>} /> 
     </Routes>
 
     <Footer/> 
---
+++ src/pages/nosotros/Nosotros.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./nosotros.css"
+import videoRuca from "../../assets/videoRuca.mp4"
+import titulo from "../../assets/RUCATEXTIL.png"
+const Nosotros = () => {
+  return (
+    <div className='nosotrosContainer'>
+        <div className='imgContainer'>
+            <img src={titulo} alt="" />
+        </div>
+        
+            <video className='videoContainer' autoPlay loop muted >
+                <source src={videoRuca} type="video/mp4" />
+            </video>
+        
+        <div className='textoContainer' >
+            <h3>NUESTRO PROCESO</h3>
+        
+        
+        <p>
+        Somos una empresa dedicada a fabricar indumentaria personalizada al por mayor con una amplia variedad de telas, diseños, talles y colores.<br/>
+        Elaboramos productos textiles innovadores de alta calidad según las necesidades de nuestros clientes, además brindamos servicio de confección completa para tu local o marca.
+        </p>
+        </div>
+    </div>
+  )
+}
+
+export default Nosotros
