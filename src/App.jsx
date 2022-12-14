da802436a26f1ff5b9646e77e87674a0c0337953
agupeiretti <agupeiretti@gmail.com>
2022-12-14 10:12:57 -0300

    14/12  agus

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import Menu from './components/Menu/Menu';
 import Home from './pages/home/Home';
-// import Productos from './pages/productos/Productos';
+import Productos from './pages/productos/Productos';
+
 
 function App() {
   return (
@@ -13,10 +14,10 @@ function App() {
     
     <Routes>
       <Route path='/' element={<Home/>} />
-      {/* <Route path='/productos' element={<Productos/>} /> */}
+      <Route path='/productos' element={<Productos/>} /> 
     </Routes>
 
-    {/* <Footer/> */}
+    <Footer/> 
    </>
   );
 }
